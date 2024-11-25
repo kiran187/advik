bab831c6ad62bb5e582959ff4320444c46629648
kiran187 <kirantupe1112@gmail.com>
2024-11-25 15:00:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/",userroute);
 app.use("/admin",adminroute);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
