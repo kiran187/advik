0f59dc08f6cfd60d567e68f1b1ca93df12d517f4
kiran187 <kirantupe1112@gmail.com>
2024-11-27 15:23:30 +0530

    Update index.js and popular_courses.ejs

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/",userroute);
 app.use("/admin",adminroute);
 
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
